9400221a99f2254a8df4a865e874e7b439b246c3
breelindsey <35509667+breelindsey@users.noreply.github.com>
2018-07-30 22:29:10 -0500

    There are changes, apparently...

--- CG2-2/Program.cs
+++ CG2-2/Program.cs
@@ -6,7 +6,20 @@ namespace CG2_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            Console.WriteLine("This program will calculate the area of a rectangle for you.");
+            Console.Write("Enter the length of side A: ");
+            string sideA = Console.ReadLine();
+            Console.Write("Enter the length of the side B: ");
+            string sideB = Console.ReadLine();
+
+            int sideAnumber = int.Parse(sideA);
+            int sideBnumber = int.Parse(sideB);
+
+            int area = sideAnumber * sideBnumber;
+
+            Console.WriteLine("The area of the rectangle is:" + area);
+            Console.ReadLine();
         }
     }
 }
